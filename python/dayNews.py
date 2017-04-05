c07e5e980e35da7539a651d69d8564457fb2c50e
RobyBioloid <roby.bioloid@gmail.com>
2017-04-05 23:10:40 +0200

    Add files via upload

---
+++ python/dayNews.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import os
+import lxml.html
+import requests
+from datetime import date
+
+
+PLUGIN_PATH = os.getcwd() + "/plugins/jarvis-leMonde"
+
+nomRubrique = ["international","politique","societe","economie","culture","idees",
+            "planete","sport","sciences","pixels","campus","m-le-mag"]
+
+lireRubrique = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+
+file = open(PLUGIN_PATH + "/config.sh")
+lines = file.readlines()
+file.close()
+
+def dayNews() :
+    i = 4
+    while i < len(lines) :
+        line = lines[i]
+        if line[len(line)-6:len(line)-2] == "True" :
+            lireRubrique[i-4] = 1
+        i += 1
+
+    reply = "Les actualités pour aujourd'hui sont :\n\n"
+    i = 0
+    while i < len(nomRubrique) :
+        rubriqueOk = False
+        if lireRubrique[i] == 1 :
+            res = requests.get("http://www.lemonde.fr/" + nomRubrique[i] + "/rss_full.xml")
+            doc = lxml.etree.fromstring(res.content)
+            
+            for item in doc.xpath('//item') :
+                myDate = item.find('pubDate').text
+                if myDate[0:2] == date.today().ctime()[0:2] :
+                    if rubriqueOk == False :
+                        reply += "Rubrique " + nomRubrique[i] + ":\n\n"
+                    rubriqueOk = True
+                    reply += item.find('title').text.encode('utf-8') + "\n"
+                    line = lines[18]
+                    if line[len(line)-6:len(line)-2] == "True" :
+                        reply += item.find('description').text.encode('utf-8') + "\n\n"
+        i += 1
+        
+    file = open(PLUGIN_PATH + "/python/news.txt", "w")
+    file.write(reply)
+    file.close
+
+
+dayNews()
---
+++ python/news.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import os
+import lxml.html
+import requests
+from datetime import date
+
+PLUGIN_PATH = os.getcwd() + "/plugins/jarvis-leMonde"
+
+
+def getRubrique(rubrique) :
+    if rubrique == "international" or rubrique == "national" :
+        return "international"
+    elif rubrique == "politique" or rubrique == "tique" :
+        return "politique"
+    elif rubrique == "societe" :
+        return "societe"
+    elif rubrique == "economie" or rubrique == "eco" or rubrique == "momie" :
+        return "economie"
+    elif rubrique == "culture" or rubrique == "le cul" :
+        return "culture"
+    elif rubrique == "idees" or rubrique == "id" :
+        return "idees"
+    elif rubrique == "planete" or rubrique == "pleine" or rubrique == "plane" or rubrique == "plaine" :
+        return "planete"
+    elif rubrique == "sport" :
+        return "sport"
+    elif rubrique == "science" or rubrique == "sciences" or rubrique == "chien" :
+        return "sciences"
+    elif rubrique == "pixels" or rubrique == "pixel" or rubrique == "sel" :
+        return "pixels"
+    elif rubrique == "campus" or rubrique == "quand tu" or rubrique == "puce" :
+        return "campus"
+    elif rubrique == "le mag" or rubrique == "le mat" or rubrique == "le mal" :
+        return "m-le-mag"
+    else :
+        return "non reconnu"
+        
+def news() :
+    file = open(PLUGIN_PATH + "/python/rubrique.txt")
+    line = file.readline()
+    line = line[0:len(line)-1]
+    file.close()
+    
+    
+    file = open(PLUGIN_PATH + "/config.sh")
+    lines = file.readlines()
+    file.close()
+
+    print (line)
+    rubrique = getRubrique(line)
+    print(rubrique)
+    if rubrique != "non reconnu" :
+        reply = "Les actualités concernant la rubrique " + rubrique + " sont :\n\n"
+        
+        res = requests.get("http://www.lemonde.fr/" + rubrique + "/rss_full.xml")
+        doc = lxml.etree.fromstring(res.content)
+                
+        for item in doc.xpath('//item') :
+            myDate = item.find('pubDate').text
+            if myDate[0:2] == date.today().ctime()[0:2] :
+                reply += item.find('title').text.encode('utf-8') + "\n"
+                line = lines[18]
+                if line[len(line)-6:len(line)-2] == "True" :
+                    reply += item.find('description').text.encode('utf-8') + "\n"
+        
+            
+        file = open(PLUGIN_PATH + "/python/news.txt", "w")
+        file.write(reply)
+        file.close
+
+news()
