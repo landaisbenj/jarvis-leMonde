6576171df6be8b1976277198e7a8a7a6149fe51f
RobyBioloid <roby.bioloid@gmail.com>
2017-04-18 17:20:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 # Use only if you need to perform changes on the user system such as installing software
 
 sudo dpkg --configure -a
-sudo apt-get install python-lxml
+sudo apt-get install python3-lxml
 sudo apt-get install requests
