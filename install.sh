b511e2bfbfc04bd236bfbf46b056ac49895c3efc
landaisbenj <32144431+landaisbenj@users.noreply.github.com>
2017-11-26 16:17:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
-
 sudo dpkg --configure -a
 sudo apt-get install python3-lxml
 sudo apt-get install requests
